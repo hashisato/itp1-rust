956f0085f0408e58ae11c4a4f9b666f36a880f76
hashisato <satoki1hashimoto@gmail.com>
2024-11-29 03:03:36 +0900

    ITP1_9_Cの提出

---
+++ ITP1_09/ITP1_9_C.rs
@@ -0,0 +1,29 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let scores: [usize; 2] = card_game();
+    println!("{} {}", scores[0], scores[1]);
+}
+
+fn card_game() -> [usize; 2] {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+    let num: usize = lines.next().unwrap().unwrap().trim().parse().unwrap();
+    let mut scores: [usize; 2] = [0, 0];
+    for _ in 0..num {
+        let mut line = lines.next().unwrap().unwrap();
+        let trimmed_line = line.trim();
+        let words = trimmed_line.split_whitespace().collect::<Vec<&str>>();
+        if words[0].to_lowercase() > words[1].to_lowercase() {
+            scores[0] += 3;
+        }
+        else if words[0].to_lowercase() == words[1].to_lowercase() {
+            scores[0] += 1;
+            scores[1] += 1;
+        }
+        else {
+            scores[1] += 3;
+        }
+    }
+    scores
+}
