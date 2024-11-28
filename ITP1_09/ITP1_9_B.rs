547aa96a9531e243bff12e6e4ea3f9e78fa8e643
hashisato <satoki1hashimoto@gmail.com>
2024-11-29 02:00:19 +0900

    ITP1_9_Bの提出

---
+++ ITP1_09/ITP1_9_B.rs
@@ -0,0 +1,29 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    input();
+}
+
+fn input() {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+    while let Some(Ok(line)) = lines.next() {
+        let initial_order = line.trim().to_string();
+        if initial_order == "-" { break; }
+        let m: usize = lines.next().unwrap().unwrap().trim().parse().unwrap();
+        let mut current_order = initial_order;
+        for _ in 0..m {
+            if let Some(Ok(line)) = lines.next() {
+                let h: usize = line.trim().parse().unwrap();
+                current_order = shuffle(&current_order, h);
+            }
+        }
+        println!("{}", current_order);
+    }
+}
+
+fn shuffle(order: &str, h: usize) -> String {
+    let len = order.len();
+    let (left, right) = order.split_at(h);
+    format!("{}{}", right, left)
+}
