c5789e6001100529216f5732996920a3ac87eb55
hashisato <satoki1hashimoto@gmail.com>
2025-05-27 14:20:02 +0900

    Update itp1_02_D.rs

--- itp1-02/itp1_02_D.rs
+++ itp1-02/itp1_02_D.rs
@@ -1,32 +1,28 @@
 fn main(){
-    let (mut W,mut H,mut x,mut y,mut r) = input();
-    println!("{}",check(W,H,x,y,r));
+    let (mut w,mut h,mut x,mut y,mut r) = input();
+    println!("{}",check(w,h,x,y,r));
 }
 
 fn input() -> (i32,i32,i32,i32,i32){
     let mut data: [i32; 5] = [0,0,0,0,0];
     let mut input = String::new();
-    std::io::stdin().read_line(&mut input)
-        .unwrap_or_else(|err| {
-            println!("Failed to read line: {}", err);0
-        });
-    let mut iter = input.split_whitespace();
-    let mut i = 0;
-    loop{
-        data[i] = iter.next().unwrap().parse()
-        .unwrap_or_else(|err| {
-            println!("Index entered was not a number: {}", err);0
-        });
-        i = i+1;
-        if(i == 5){ break; }
-    }
-    return (data[0],data[1],data[2],data[3],data[4]);
+    std::io::stdin().read_line(&mut input).expect("Failed to read line");
+    let v: Vec<i32> = input.split_whitespace()
+                        .map(|s| s.trim().parse().unwrap_or(0))
+                        .collect();
+    return (v[0],v[1],v[2],v[3],v[4]);
 }
 
-fn check(W: i32,H: i32,x: i32,y: i32,r: i32) -> String{
+fn check(w: i32,h: i32,x: i32,y: i32,r: i32) -> String{
     if(0<=(x-r) && 0<=(y-r)){
-        if((x+r)<=W && (y+r)<=H){ return "Yes".to_string(); }
-        else{ return "No".to_string(); }
+        if((x+r)<=w && (y+r)<=h){ 
+            return "Yes".to_string();
+        }
+        else{
+            return "No".to_string();
+        }
     }
-    else{ return "No".to_string(); }
+    else{
+        return "No".to_string();
+    }
 }
