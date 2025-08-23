6d42035ef0b47c5dbb5c73e9941dfaea230a134c
hashisato <satoki1hashimoto@gmail.com>
2025-08-23 11:41:32 +0900

    Update itp1_01_C.rs

--- itp1-01/itp1_01_C.rs
+++ itp1-01/itp1_01_C.rs
@@ -1,13 +1,13 @@
 fn main(){
     let (mut a, mut b) = input();
-    println!("{} {}", a*b,2*(a+b));
+    println!("{} {}", a*b, 2*(a+b));
 }
 
-fn input() -> (i32, i32){
+fn input() -> (usize, usize){
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
     let mut iter = input.split_whitespace();
-    let a: i32 = iter.next().unwrap().parse().unwrap();
-    let b: i32 = iter.next().unwrap().parse().unwrap();
+    let a: usize = iter.next().unwrap().parse().unwrap();
+    let b: usize = iter.next().unwrap().parse().unwrap();
     (a, b)
 }
