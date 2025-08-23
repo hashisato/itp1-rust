2aa19f0d713f49e70e22cccb0cf20b4c9c4f79a4
hashisato <satoki1hashimoto@gmail.com>
2025-08-23 15:04:26 +0900

    Update itp1_02_D.rs

--- itp1-02/itp1_02_D.rs
+++ itp1-02/itp1_02_D.rs
@@ -1,26 +1,22 @@
 fn main(){
-    let (mut w,mut h,mut x,mut y,mut r) = input();
-    println!("{}",check(w,h,x,y,r));
+    let mut num = input();
+    println!("{}", check(num[0], num[1], num[2], num[3], num[4]));
 }
 
-fn input() -> (i32,i32,i32,i32,i32){
-    let mut data: [i32; 5] = [0,0,0,0,0];
+fn input() -> Vec<i32> {
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
-    let v: Vec<i32> = input.split_whitespace().map(|s| s.parse().unwrap()).collect();
-    return (v[0],v[1],v[2],v[3],v[4])
+    input.split_whitespace().map(|s| s.parse().unwrap()).collect()
 }
 
-fn check(w: i32,h: i32,x: i32,y: i32,r: i32) -> String{
-    if(0<=(x-r) && 0<=(y-r)){
-        if((x+r)<=w && (y+r)<=h){ 
-            return "Yes".to_string();
-        }
-        else{
-            return "No".to_string();
-        }
-    }
-    else{
-        return "No".to_string();
+fn check(w: i32, h: i32, x: i32, y: i32, r: i32) -> String{
+    match (
+        0 <= x - r,
+        0 <= y - r,
+        x + r <= w,
+        y + r <= h
+    ) {
+        (true, true, true, true) => "Yes".to_string(),
+        _ => "No".to_string(),
     }
 }
