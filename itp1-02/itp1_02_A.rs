74aaf223c1c95d509d6b3d68876051dfc9aca52d
hashisato <satoki1hashimoto@gmail.com>
2025-08-23 12:01:22 +0900

    Update itp1_02_A.rs

--- itp1-02/itp1_02_A.rs
+++ itp1-02/itp1_02_A.rs
@@ -1,25 +1,21 @@
 fn main(){
-    let (mut a,mut b) = input();
+    let (a, b) = input();
     println!("a {} b",compare(a,b));
 }
 
-fn input() -> (i32,i32){
+fn input() -> (i32, i32){
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
     let mut iter = input.split_whitespace();
     let a: i32 = iter.next().unwrap().parse().unwrap();
     let b: i32 = iter.next().unwrap().parse().unwrap();
-    return (a,b)
+    (a, b)
 }
 
 fn compare(a: i32,b: i32) -> String{
-    if(a<b){
-        return "<".to_string();
-    }
-    else if(a>b){
-        return ">".to_string();
-    }
-    else{
-        return "==".to_string();
+    match a.cmp(&b) {
+        std::cmp::Ordering::Less => "<".to_string(),
+        std::cmp::Ordering::Greater => ">".to_string(),
+        std::cmp::Ordering::Equal => "==".to_string(),
     }
 }
