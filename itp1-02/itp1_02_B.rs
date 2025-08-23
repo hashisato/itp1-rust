1604d96f92199a45b9aa1c765f735b45c2e4e7ac
hashisato <satoki1hashimoto@gmail.com>
2025-08-23 15:04:19 +0900

    Update itp1_02_B.rs

--- itp1-02/itp1_02_B.rs
+++ itp1-02/itp1_02_B.rs
@@ -1,22 +1,15 @@
 fn main(){
-    let (mut a,mut b,mut c) = input();
-    println!("{}",compare(a,b,c));
+    let (a, b, c) = input();
+    if a<b && b<c { println!("Yes"); }
+    else { println!("No"); }
 }
 
-fn input() -> (i32,i32,i32){
+fn input() -> (i32, i32, i32){
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
     let mut iter = input.split_whitespace();
     let a: i32 = iter.next().unwrap().parse().unwrap();
     let b: i32 = iter.next().unwrap().parse().unwrap();
     let c: i32 = iter.next().unwrap().parse().unwrap();
-    return (a,b,c)
-}
-
-fn compare(a: i32,b: i32,c: i32) -> String{
-    if(a<b && b<c){
-        return "Yes".to_string()
-    } else {
-        return "No".to_string()
-    }
+    (a, b, c)
 }
