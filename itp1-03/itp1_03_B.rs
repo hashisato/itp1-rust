38605356183d68a9903b173ef06ea15e1fcd4f92
hashisato <satoki1hashimoto@gmail.com>
2025-08-23 15:28:05 +0900

    Update itp1_03_B.rs

--- itp1-03/itp1_03_B.rs
+++ itp1-03/itp1_03_B.rs
@@ -1,17 +1,9 @@
-fn main(){
-    testCase();
-}
-
-fn testCase(){
-    let mut i: i32 = 0;
-    loop{
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input).unwrap();
-        let mut x = input.trim().parse().unwrap_or_else(|_| 0);
+fn main() {
+    let mut case = 1;
+    for line in std::io::stdin().lines() {
+        let x: i32 = line.unwrap().trim().parse().unwrap();
         if x == 0 { break; }
-        else{
-            i = i + 1;
-            println!("Case {}: {}",i,x);
-        }
+        println!("Case {}: {}", case, x);
+        case += 1;
     }
 }
