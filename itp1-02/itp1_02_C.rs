20058afc0c8c9a01a4767fdfcad0dad6c9c788f8
hashisato <satoki1hashimoto@gmail.com>
2025-08-23 15:04:23 +0900

    Update itp1_02_C.rs

--- itp1-02/itp1_02_C.rs
+++ itp1-02/itp1_02_C.rs
@@ -1,16 +1,11 @@
 fn main(){
-    let (mut a,mut b,mut c) = input();
-    let mut numbers = vec![a,b,c];
+    let mut numbers = input();
     numbers.sort();
     println!("{} {} {}",numbers[0],numbers[1],numbers[2]);
 }
 
-fn input() -> (i32,i32,i32){
+fn input() -> Vec<i32> {
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
-    let mut iter = input.split_whitespace();
-    let a: i32 = iter.next().unwrap().parse().unwrap();
-    let b: i32 = iter.next().unwrap().parse().unwrap();
-    let c: i32 = iter.next().unwrap().parse().unwrap();
-    return (a,b,c)
+    input.split_whitespace().map(|s| s.parse().unwrap()).collect()
 }
